caf729c27b755293fd76c409bd01462946d35718
Kat Maldon <katharinamaldon@gmail.com>
2019-07-22 17:32:52 +0000

    user controller signup login logout

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -9,18 +9,39 @@ class UserController < ApplicationController
   end
 
   post "/signup" do
+    user = User.new(:email => params[:email], :password => params[:password])
+    if user.save 
+      session[:user_id] = user.id 
+      redirect "/home"
+    else 
+      flash[:error] = "Sign up failed. Please try again."
+      redirect "/signup"
+    end 
   end
-
 
   get "/login" do
     erb :login
   end
 
   post "/login" do
+    user = User.find_by(:email => params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id 
+      redirect "/home"
+    else 
+      flash[:error] = "The user name or password did not match. Please try again."
+      redirect "/login"
+    end 
   end
 
   get "/logout" do
+    if logged_in?
+      session.clear 
+      redirect "/login"
+    else 
+      flash[:error] = "Unable to fulfill request. Please try again."
+      redirect "/login"
+    end 
   end
-
 
 end

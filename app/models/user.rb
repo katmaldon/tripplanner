df8e5a83b2dc65880f95b41b9df5ea0ce86fc053
Kat Maldon <katharinamaldon@gmail.com>
2019-07-22 18:01:02 +0000

    model user filled in

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
-class user < ActiveRecord::Base 
-  # password validation, format etc 
-end 
+class User < ActiveRecord::Base
+  has_secure_password
+  has_many :trips
+  validates_presence_of :password 
+  validates :email, :presence => true,
+                    :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/ }
+end

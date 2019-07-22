1c107695616917ddf5f625075cf95707b7e07540
Kat Maldon <katharinamaldon@gmail.com>
2019-07-22 16:43:10 +0000

    app controller

---
+++ app/controllers/app_controller.rb
@@ -0,0 +1,26 @@
+require './config/environment'
+require 'sinatra'
+require 'sinatra/flash'
+
+class AppController < Sinatra::Base 
+  
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    enable :session 
+    set :sessions_secret, "password_security"
+    register Sinatra::Flash
+  end 
+  
+  get "/" do 
+    erb :index 
+  end
+  
+  helpers do 
+    
+    def logged_in?
+      !!session[:user_id])
+    end 
+  end 
+end 
+    
